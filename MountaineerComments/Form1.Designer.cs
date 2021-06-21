a43473ff0f89289a42491463e4e87cadf73a5d60
eroliver <eroliver27@hotmail.com>
2021-06-21 16:03:16 -0400

    Add project files.

---
+++ MountaineerComments/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿
+
+namespace MountaineerComments
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.Comment = new System.Windows.Forms.TextBox();
+            this.SendButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.CommenterName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.NameandCommentPanel = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.NameandCommentPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(89, 83);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Comment*";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Comment
+            // 
+            this.Comment.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.Comment.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.Comment.Font = new System.Drawing.Font("Century", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Comment.Location = new System.Drawing.Point(89, 106);
+            this.Comment.Multiline = true;
+            this.Comment.Name = "Comment";
+            this.Comment.Size = new System.Drawing.Size(700, 213);
+            this.Comment.TabIndex = 1;
+            this.Comment.TextChanged += new System.EventHandler(this.CommentBox_TextChanged);
+            // 
+            // SendButton
+            // 
+            this.SendButton.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.SendButton.Location = new System.Drawing.Point(685, 330);
+            this.SendButton.Name = "SendButton";
+            this.SendButton.Size = new System.Drawing.Size(104, 45);
+            this.SendButton.TabIndex = 2;
+            this.SendButton.Text = "Send";
+            this.SendButton.UseVisualStyleBackColor = true;
+            this.SendButton.Click += new System.EventHandler(this.SendButton_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(716, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(472, 199);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // CommenterName
+            // 
+            this.CommenterName.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.CommenterName.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.CommenterName.Font = new System.Drawing.Font("Century", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.CommenterName.Location = new System.Drawing.Point(89, 41);
+            this.CommenterName.Name = "CommenterName";
+            this.CommenterName.Size = new System.Drawing.Size(700, 27);
+            this.CommenterName.TabIndex = 5;
+            this.CommenterName.TextChanged += new System.EventHandler(this.CommenterNamesucks_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Location = new System.Drawing.Point(89, 6);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Name*";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(84, 330);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(94, 16);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "*required fields";
+            // 
+            // NameandCommentPanel
+            // 
+            this.NameandCommentPanel.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.NameandCommentPanel.Controls.Add(this.label3);
+            this.NameandCommentPanel.Controls.Add(this.label2);
+            this.NameandCommentPanel.Controls.Add(this.CommenterName);
+            this.NameandCommentPanel.Controls.Add(this.SendButton);
+            this.NameandCommentPanel.Controls.Add(this.Comment);
+            this.NameandCommentPanel.Controls.Add(this.label1);
+            this.NameandCommentPanel.Location = new System.Drawing.Point(529, 417);
+            this.NameandCommentPanel.MinimumSize = new System.Drawing.Size(750, 320);
+            this.NameandCommentPanel.Name = "NameandCommentPanel";
+            this.NameandCommentPanel.Size = new System.Drawing.Size(846, 455);
+            this.NameandCommentPanel.TabIndex = 8;
+            // 
+            // textBox1
+            // 
+            this.textBox1.AccessibleDescription = "Call to action from the HR department to share thoughts and suggestions about the" +
+    " company.";
+            this.textBox1.AccessibleName = "Invitation to comment";
+            this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.textBox1.BackColor = System.Drawing.SystemColors.Desktop;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.textBox1.Font = new System.Drawing.Font("Century", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox1.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.textBox1.Location = new System.Drawing.Point(529, 231);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(0);
+            this.textBox1.MinimumSize = new System.Drawing.Size(300, 107);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(846, 192);
+            this.textBox1.TabIndex = 9;
+            this.textBox1.Text = resources.GetString("textBox1.Text");
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.SystemColors.InfoText;
+            this.ClientSize = new System.Drawing.Size(1904, 1061);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.NameandCommentPanel);
+            this.Controls.Add(this.pictureBox1);
+            this.Font = new System.Drawing.Font("Century", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.MaximumSize = new System.Drawing.Size(1920, 1149);
+            this.MinimumSize = new System.Drawing.Size(800, 531);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Comments";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.NameandCommentPanel.ResumeLayout(false);
+            this.NameandCommentPanel.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Comment;
+        private System.Windows.Forms.Button SendButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox CommenterName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel NameandCommentPanel;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ MountaineerComments/Form1.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Net;
+
+using System.Net.Mail;
+using System.Text.RegularExpressions;
+
+namespace MountaineerComments
+{
+    public partial class Form1 : Form
+    {
+        MailMessage message;
+        SmtpClient smtp;
+        string commentRecipient = "mtr.misdepartment@cnty.com";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SendButton_Click(object sender, EventArgs e)
+        {
+            try
+
+            {
+
+                SendButton.Enabled = false;
+
+
+                message = new MailMessage();
+
+
+                //no need for this, we will pass the email
+                //if (IsValidEmail(txtTo.Text))
+
+                //{
+
+                message.To.Add(commentRecipient);
+
+                //}
+
+                //if (IsValidEmail(txtCC.Text))
+
+                //{
+
+                //message.CC.Add(txtCC.Text);
+
+                //}
+
+
+                message.Subject = "Message from MountaineerComments Application";
+
+                message.From = new MailAddress("mountaineercomments@cnty.com");
+
+                if (CheckField(Comment) && CheckField(CommenterName))
+                {
+
+                    string comment = ($"{CommenterName.Text}:    {Comment.Text}");
+                    message.Body = comment;
+                    this.Refresh();
+                }
+                else
+                {
+                    MessageBox.Show("Name and Comment are BOTH required" +
+                        "\n and can NOT contain 5 consecutive characters.");
+
+
+                    SendButton.Enabled = true;
+
+                    ResetFields();
+
+                    return;
+                }
+
+                //if (lblAttachment.Text.Length > 0)
+
+                //{
+
+                //if (System.IO.File.Exists(lblAttachment.Text))
+
+                //{
+
+                //message.Attachments.Add(new Attachment(lblAttachment.Text));
+
+                //}
+
+                //}
+
+
+
+                // set smtp details
+
+                smtp = new SmtpClient("smtp-relay.gmail.com");
+
+                smtp.Port = 25;
+
+                smtp.EnableSsl = false;
+
+                //smtp.Credentials = new NetworkCredential(emailSender, "nsmldcwqslfijndu");
+
+                smtp.SendAsync(message, message.Subject);
+
+                smtp.SendCompleted += new SendCompletedEventHandler(smtp_SendCompleted);
+
+                ResetFields();
+
+            }
+
+            catch (Exception ex)
+
+            {
+
+                MessageBox.Show(ex.Message);
+
+
+                SendButton.Enabled = true;
+
+            }
+        }
+
+        private void CommentBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        void smtp_SendCompleted(object sender, AsyncCompletedEventArgs e)
+
+        {
+
+            if (e.Cancelled == true)
+
+            {
+
+                MessageBox.Show("Comment sending cancelled!");
+
+            }
+
+            else if (e.Error != null)
+
+            {
+
+                MessageBox.Show(e.Error.Message);
+
+            }
+
+            else
+
+            {
+
+                MessageBox.Show("Comment sent sucessfully! Thank you!");
+
+            }
+
+
+
+
+            SendButton.Enabled = true;
+
+        }
+
+
+
+        private void btnCancel_Click(object sender, EventArgs e)
+
+        {
+
+            smtp.SendAsyncCancel();
+
+            MessageBox.Show("Comment sending cancelled!");
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private bool CheckField(TextBox textBox)
+        {
+            //Verifies user input in text fields, checking for empty string or repeating characters.
+            if (textBox.Text != String.Empty && !HasConsecutiveChars(textBox.Text, 5))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            
+        }
+
+        public static bool HasConsecutiveChars(string source, int sequenceLength)
+        {
+            // should check sequence length, and return true if it finds consecutive repeating characters.
+
+            // just repeating letters
+            //return Regex.IsMatch(source, "([a-zA-Z])\\1{" + (sequenceLength - 1) + "}");
+
+            // any character version
+            return Regex.IsMatch(source,"(.)\\1{"+ (sequenceLength - 1) + "}");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CommenterNamesucks_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ResetFields()
+        {
+            Comment.Text = string.Empty;
+            CommenterName.Text = string.Empty;
+
+            //foreach (Control control in this.Controls)
+            //{
+            //    if (control is TextBox)
+            //        if ((control as TextBox).Text != string.Empty)
+            //        {
+            //            Text = string.Empty;
+            //        }
+            //}
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        //SmtpClient.SendAsyncCancel() method is used to cancel the email send operation.
+
+
+
+        //implement to add attachments along with message
+        //private void lnkAttachFile_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+
+        //{
+
+        //if (openFileDialog1.ShowDialog() != DialogResult.Cancel)
+
+        //{
+
+        //lblAttachment.Text = openFileDialog1.FileName;
+
+        //lblAttachment.Visible = true;
+
+        //lnkAttachFile.Visible = false;
+
+        //}
+    }
+}
+
